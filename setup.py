93f76eacb9ef3e48cac8ec67a6caa6cb66a7683f
Patrick Burrus <Patrick.Burrus@kochind.com>
2019-03-28 09:33:02 -0500

    initial push

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+  name = 'KSTLogger',
+  packages = ['KSTLogger'], 
+  version = '0.1.0',
+  author = 'Patrick Burrus',
+  author_email = 'Patrick.burrus@kochind.com'
+)
