1283c1e483f4276e4dd454e9933528d70699c5a4
Patrick Burrus <Patrick.Burrus@kochind.com>
2019-04-29 13:23:40 -0500

    fixing additional nesting issue

--- KSTLogger/__init__.py
+++ KSTLogger/__init__.py
@@ -6,14 +6,10 @@ class KSTLogger:
 
     def log_entry(SourceProcessName, CorrelationID, logSeverity, log_detail, Additional = None):
         logMessage = None
+        logMessage = '{"SourceProcessName": "' + SourceProcessName + '", "log_timestamp_utc": "' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '", "log_severity": "' + logSeverity + '", "CorrelationID": "' + CorrelationID + '", "log_detail": "' + log_detail + '"}'
+        logMessage = json.loads(logMessage)
         if Additional != None:
-            Additional = str(Additional)
-            Additional = Additional.replace("{",", ")
-            Additional = Additional.replace("}","")
-            Additional = Additional.replace("'","\"")
-            logMessage = '{"SourceProcessName": "' + SourceProcessName + '", "log_timestamp_utc": "' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '", "log_severity": "' + logSeverity + '", "CorrelationID": "' + CorrelationID + '", "log_detail": "' + log_detail + '"' + Additional + '}'
-        else:
-            logMessage = '{"SourceProcessName": "' + SourceProcessName + '", "log_timestamp_utc": "' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '", "log_severity": "' + logSeverity + '", "CorrelationID": "' + CorrelationID + '", "log_detail": "' + log_detail + '"}'
-        json.loads(logMessage)
+            for item in Additional:
+                logMessage[item]=Additional[item]
         print(logMessage)
 
