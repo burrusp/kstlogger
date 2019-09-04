20e25fc0d56cce005f2d5034d284bdeb596046b2
Patrick Burrus <Patrick.Burrus@kochind.com>
2019-09-04 16:47:05 -0500

    testing updates

--- KSTLogger/__init__.py
+++ KSTLogger/__init__.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import json
+import traceback
 
 
 class KSTLogger:
@@ -10,34 +11,40 @@ class KSTLogger:
         self._timestamp_format=timestamp_format
 
     def log(self, log_severity, source_system_id, trade_id, source_system, log_detail, **kwargs):
-        debug = os.environ["DEBUG"]
-        log_message={
-            "log_severity": log_severity,
-            "source_process_name": self._source_process_name,
-            "log_timestamp_utc": datetime.datetime.utcnow().strftime(self._timestamp_format),
-            "log_detail": log_detail,
-            "source_system_id": source_system_id,
-            "source_system": source_system,
-            "trade_id": trade_id
-        }
-        if log_severity.lower() == "debug" and debug.lower() == "true": 
-            if kwargs:
-                for arg in kwargs:
-                    if kwargs[arg] is not None:
-                        log_message[arg]=kwargs[arg]
-                log_message=json.dumps(log_message)
-                print(log_message)
-            else:
-                log_message=json.dumps(log_message)
-                print(log_message)
-        elif log_severity.lower() != "debug":
-            if kwargs:
-                for arg in kwargs:
-                    if kwargs[arg] is not None:
-                        log_message[arg]=kwargs[arg]
-                log_message=json.dumps(log_message)
-                print(log_message)
-            else:
-                log_message=json.dumps(log_message)
-                print(log_message)
+        try:
+            try:
+                debug = os.environ["DEBUG"]
+            except:
+                debug = "false"
+            log_message={
+                "log_severity": log_severity,
+                "source_process_name": self._source_process_name,
+                "log_timestamp_utc": datetime.datetime.utcnow().strftime(self._timestamp_format),
+                "log_detail": log_detail,
+                "source_system_id": source_system_id,
+                "source_system": source_system,
+                "trade_id": trade_id
+            }
+            if log_severity.lower() == "debug" and debug.lower() == "true": 
+                if kwargs:
+                    for arg in kwargs:
+                        if kwargs[arg] is not None:
+                            log_message[arg]=kwargs[arg]
+                    log_message=json.dumps(log_message)
+                    print(log_message)
+                else:
+                    log_message=json.dumps(log_message)
+                    print(log_message)
+            elif log_severity.lower() != "debug":
+                if kwargs:
+                    for arg in kwargs:
+                        if kwargs[arg] is not None:
+                            log_message[arg]=kwargs[arg]
+                    log_message=json.dumps(log_message)
+                    print(log_message)
+                else:
+                    log_message=json.dumps(log_message)
+                    print(log_message)
+        except:
+            traceback.print_exc()
 
