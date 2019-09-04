79064d08cfb1608cf26cf09bf7fedbb952a3405b
Patrick Burrus <Patrick.Burrus@kochind.com>
2019-09-04 16:25:01 -0500

    testing updates

--- KSTLogger/__init__.py
+++ KSTLogger/__init__.py
@@ -1,7 +1,6 @@
 import datetime
 import os
 import json
-from kmt_logging import KMTLogger
 
 
 class KSTLogger:
@@ -9,19 +8,36 @@ class KSTLogger:
     def __init__(self,source_process_name=None, timestamp_format='%Y-%m-%d %H:%M:%S'):
         self._source_process_name=source_process_name
         self._timestamp_format=timestamp_format
-        global kmtlogger
-        kmtlogger=KMTLogger(self._source_process_name)
 
     def log(self, log_severity, source_system_id, trade_id, source_system, log_detail, **kwargs):
         debug = os.environ["DEBUG"]
+        log_message={
+            "log_severity": log_severity,
+            "source_process_name": self._source_process_name=source_process_name,
+            "log_timestamp_utc": datetime.datetime.utcnow().strftime(self._timestamp_format),
+            "log_detail": log_detail,
+            "source_system_id": source_system_id,
+            "source_system": source_system,
+            "trade_id": trade_id
+        }
         if log_severity.lower() == "debug" and debug.lower() == "true": 
             if kwargs:
-                kmtlogger.log(log_severity, log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, **kwargs)
+                for arg in kwargs:
+                    if kwargs[arg_key] is not None:
+                        log_message[arg_key]=kwargs[arg]
+                log_message=json.dumps(log_message)
+                print(log_message)
             else:
-                kmtlogger.log(log_severity, log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id)
+                log_message=json.dumps(log_message)
+                print(log_message)
         elif log_severity.lower() != "debug":
             if kwargs:
-                kmtlogger.log(log_severity, log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, **kwargs)
+                for arg in kwargs:
+                    if kwargs[arg_key] is not None:
+                        log_message[arg_key]=kwargs[arg]
+                log_message=json.dumps(log_message)
+                print(log_message)
             else:
-                kmtlogger.log(log_severity, log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id)
+                log_message=json.dumps(log_message)
+                print(log_message)
 
