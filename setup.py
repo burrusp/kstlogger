dcf8f04b9f830f9cba5b197c66932071c460bc11
Patrick Burrus <Patrick.Burrus@kochind.com>
2019-09-04 14:16:26 -0500

    testing updates

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
   author = 'Patrick Burrus',
   author_email = 'Patrick.burrus@kochind.com',
   install_requires=[
-        'kmt_logger @ git+https://kochsource.io/kmt-frameworks/logging-frameworks/kmt-logging.git@0.2.108.1'
+        '* @ git+https://kochsource.io/kmt-frameworks/logging-frameworks/kmt-logging.git@0.2.108.1'
     ]
 )
