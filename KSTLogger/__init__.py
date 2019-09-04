63cbea418fc7542fa4b496c8f590912bc91cd8aa
Patrick Burrus <Patrick.Burrus@kochind.com>
2019-09-04 13:13:59 -0500

    testing updates

--- KSTLogger/__init__.py
+++ KSTLogger/__init__.py
@@ -1,13 +1,21 @@
 import datetime
 import json
+from kmt_logging import KMTLogger
 
 
 class KSTLogger:
 
-    def log_entry(SourceProcessName, CorrelationID, logSeverity, log_detail, Additional = None):
+    def __init__(self,source_process_name=None, timestamp_format='%Y-%m-%d %H:%M:%S'))
+        self._source_process_name=source_process_name
+        self._timestamp_format=timestamp_format
+
+    def begin(self, lambdaname, source_system_id, trade_id, source_system):
+        kmtlogger=KMTLogger(self._source_process_name)
+        kmtlogger.log("INFO", "Beginning lambda " + lambdaname, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id)
+
+    def log_entry(log_severity, source_process_name, source_system_id, log_detail, Additional = None):
         logMessage = None
-        logMessage = '{"SourceProcessName": "' + SourceProcessName + '", "log_timestamp_utc": "' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '", "log_severity": "' + logSeverity + '", "CorrelationID": "' + CorrelationID + '", "log_detail": "' + log_detail + '"}'
-        logMessage = json.loads(logMessage)
+        logMessage = {"source_process_name": source_process_name, "log_timestamp_utc": datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), "log_severity": log_severity, "source_system_id": source_system_id, "log_detail": log_detail}
         if Additional != None:
             for item in Additional:
                 logMessage[item]=Additional[item]
--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ setup(
   packages = ['KSTLogger'], 
   version = '0.1.0',
   author = 'Patrick Burrus',
-  author_email = 'Patrick.burrus@kochind.com'
+  author_email = 'Patrick.burrus@kochind.com',
+  install_requires=[
+        "git+https://kochsource.io/kmt-frameworks/logging-frameworks/kmt-logging.git@0.2.108.1"
+    ],
 )
