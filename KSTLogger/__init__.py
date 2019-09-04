c3b1b1c5f4107db1fa8e56d0d54dcd6dd367fce2
Patrick Burrus <Patrick.Burrus@kochind.com>
2019-09-04 16:04:57 -0500

    testing updates

--- KSTLogger/__init__.py
+++ KSTLogger/__init__.py
@@ -12,29 +12,16 @@ class KSTLogger:
         global kmtlogger
         kmtlogger=KMTLogger(self._source_process_name)
 
-    def begin(self, lambda_name, trigger_file, source_system_id, trade_id, source_system, additional = None):
-        also={}
-        if additional:
-            for item in additional:
-                also[item]=additional[item]
-            kmtlogger.log("INFO", "invoked lambda " + lambda_name, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, trigger_file=trigger_file, additional=also)
-        else:
-            kmtlogger.log("INFO", "invoked lambda " + lambda_name, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, trigger_file=trigger_file)
+    def log(self, log_severity, source_system_id, trade_id, source_system, log_detail, **kwargs):
+        debug = os.environ["DEBUG"]
+        if log_severity.lower() == "debug" and debug: 
+            if kwargs:
+                kmtlogger.log(log_severity, log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, **kwargs)
+            else:
+                kmtlogger.log(log_severity, log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id)
+        elif log_severity.lower() != "debug":
+            if kwargs:
+                kmtlogger.log(log_severity, log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, **kwargs)
+            else:
+                kmtlogger.log(log_severity, log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id)
 
-    def end(self, lambda_name, destination_file, source_system_id, trade_id, source_system, additional = None):
-        also={}
-        if additional:
-            for item in additional:
-                also[item]=additional[item]
-            kmtlogger.log("INFO", "completed lambda " + lambda_name, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, destination_file=destination_file, additional=also)
-        else:
-            kmtlogger.log("INFO", "completed lambda " + lambda_name, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, destination_file=destination_file)
-
-    def log(self, log_severity, source_system_id, trade_id, source_system, log_detail, additional = None):
-        also={}
-        if additional:
-            for item in additional:
-                also[item]=additional[item]
-            kmtlogger.log(log_severity, log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, additional=also)
-        else:
-            kmtlogger.log(log_severity, log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id)
