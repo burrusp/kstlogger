4155d06d7f77212221deda5175554c8bce11b8fd
Patrick Burrus <Patrick.Burrus@kochind.com>
2019-09-04 14:57:13 -0500

    testing updates

--- KSTLogger/__init__.py
+++ KSTLogger/__init__.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 import json
 from kmt_logging import KMTLogger
 
@@ -14,12 +15,51 @@ class KSTLogger:
     def begin(self, lambda_name, trigger_file, source_system_id, trade_id, source_system):
         kmtlogger.log("INFO", "invoked lambda " + lambda_name, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, trigger_file=trigger_file)
 
-    def log_entry(log_severity, source_process_name, source_system_id, log_detail, Additional = None):
-        logMessage = None
-        logMessage = {"source_process_name": source_process_name, "log_timestamp_utc": datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"), "log_severity": log_severity, "source_system_id": source_system_id, "log_detail": log_detail}
-        if Additional != None:
-            for item in Additional:
-                logMessage[item]=Additional[item]
-        logMessage = json.dumps(logMessage)
-        print(logMessage)
+    def end(self, lambda_name, destination_file, source_system_id, trade_id, source_system):
+        kmtlogger.log("INFO", "invoked lambda " + lambda_name, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, destination_file=destination_file)
+
+    def info(self, source_system_id, trade_id, source_system, log_detail, additional = None):
+        also={}
+        if additional:
+            for item in additional:
+                also[item]=additional[item]
+        kmtlogger.log("INFO", log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, additional=also)
+
+    def debug(self, source_system_id, trade_id, source_system, log_detail, additional = None):
+        #debug = os.environ["DEBUG"]
+        debug = "TRUE"
+        if debug.lower() = "true":
+            also={}
+            if additional:
+                for item in additional:
+                    also[item]=additional[item]
+            kmtlogger.log("DEBUG", log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, additional=also)
+
+    def buserror(self, source_system_id, trade_id, source_system, log_detail, additional = None):
+        also={}
+        if additional:
+            for item in additional:
+                also[item]=additional[item]
+        kmtlogger.log("ERROR", log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, error_type="business_error", additional=also)
+
+    def techerror(self, source_system_id, trade_id, source_system, log_detail, additional = None):
+        also={}
+        if additional:
+            for item in additional:
+                also[item]=additional[item]
+        kmtlogger.log("ERROR", log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, error_type="technical_error", additional=also)
+
+    def error(self, source_system_id, trade_id, source_system, log_detail, additional = None):
+        also={}
+        if additional:
+            for item in additional:
+                also[item]=additional[item]
+        kmtlogger.log("ERROR", log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, error_type="unhandled_error", additional=also)
+
+    def warn(self, source_system_id, trade_id, source_system, log_detail, additional = None):
+        also={}
+        if additional:
+            for item in additional:
+                also[item]=additional[item]
+        kmtlogger.log("warn", log_detail, source_system_id=source_system_id, source_system=source_system, trade_id=trade_id, additional=also)
 
