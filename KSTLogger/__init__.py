d1419b8bf613b39f80b84e675b367b14f64f8779
burrusp <49033105+burrusp@users.noreply.github.com>
2021-05-18 12:22:59 -0500

    Update __init__.py

--- KSTLogger/__init__.py
+++ KSTLogger/__init__.py
@@ -6,8 +6,13 @@ class KSTLogger:
 
     def log_entry(SourceProcessName, CorrelationID, logSeverity, log_detail, Additional = None):
         logMessage = None
-        logMessage = '{"SourceProcessName": "' + SourceProcessName + '", "log_timestamp_utc": "' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + '", "log_severity": "' + logSeverity + '", "CorrelationID": "' + CorrelationID + '", "log_detail": "' + log_detail + '"}'
-        logMessage = json.loads(logMessage)
+        logMessage = {
+            "SourceProcessName": SourceProcessName, 
+            "log_timestamp_utc": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 
+            "log_severity": logSeverity, 
+            "CorrelationID": CorrelationID, 
+            "log_detail": log_detail
+        }
         if Additional != None:
             for item in Additional:
                 logMessage[item]=Additional[item]
